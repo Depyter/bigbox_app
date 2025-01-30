0dbf15708cf467ca2c76913861708b7206f39eba
Harley Van Acabal <developer@harleyvan.com>
2025-01-31 00:04:56 +0800

    init commit and basic setup

---
+++ android/app/src/main/kotlin/com/example/bigbox_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bigbox_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/data/database.dart
@@ -0,0 +1,1 @@
+// This is where we define table schemas and initialize Sqlite database
